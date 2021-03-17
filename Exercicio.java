da0f96de8ab822f112102d646eae9e1af5fcc7ba
Milena <Milena@192.168.0.102>
2021-03-17 09:45:56 -0300

    Termino até o ex03(televisão) + teste de push

--- Exercicio.java
+++ Exercicio.java
@@ -5,34 +5,55 @@ import java.util.Scanner;
 public class Exercicio {
 	
 	public static class Livro {
+		//Atributos:
 		String titulo;
 		String autor;
 		int quantidadePaginas;
 		
 		public Livro (String titulo, String autor, int quantidadePaginas) {
+			//Construtor:
 			this.titulo = titulo;
 			this.autor = autor;
 			this.quantidadePaginas = quantidadePaginas;
 		
 	}
 		public static class Triangulo {
-			
+			//Atributos:
 			double ladoA, ladoB, ladoC;
 			
 			public Triangulo(double ladoA, double ladoB, double ladoC) {
+				//Construtor:
 				this.ladoA = ladoA;
 				this.ladoB = ladoB;
 				this.ladoC = ladoC;
 			}
-			
+			//Método01:
 			public double calcularPerimetro() {
 				return this.ladoA + this.ladoB + this.ladoC;
 			}
 		}
 		public static class Televisao {
+			//Atributos:
+			int canal;
+			int volume;
 			
-			int canal = 0;
-			int volume = 0;
+			//Contrutor
+			public Televisao() {
+				this.canal = 0;
+				this.volume = 0;
+			}
+			
+			public void aumentarVolume() {
+				this.volume++;
+			}
+			
+			public void diminuirVolume( ) {
+				this.volume--;
+			}
+			
+			public void alterarCanal(int canal) {
+				this.canal = canal;
+			}
 			
 		}
 
@@ -44,12 +65,24 @@ public class Exercicio {
 		Scanner entrada = new Scanner(System.in);
 		double a, b, c;
 		
+		System.out.print("Informe os lados do triângulo:");
 		a = entrada.nextDouble();
 		b = entrada.nextDouble();
 		c = entrada.nextDouble();
 		
+		//Criação do objeto triângulo:
 		Triangulo t = new Triangulo(a, b, c);
 		System.out.println("Perímetro: " + t.calcularPerimetro());
+		
+		Televisao tv = new Televisao();
+		tv.alterarCanal(5);
+		tv.aumentarVolume();
+		tv.aumentarVolume();
+		tv.aumentarVolume();
+		tv.diminuirVolume();
+		System.out.println("A tv está no canal " + tv.canal);   // A tv está no canal 5
+		System.out.println("A tv está no volume " + tv.volume); // A tv está no volume 2
+
 
 	}
 
